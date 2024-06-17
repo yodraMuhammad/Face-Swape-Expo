95d45195dce52751afd9efe4929a50861d36e018
Yodra Muhamad Akbar N <56280668+yodraMuhammad@users.noreply.github.com>
2024-06-18 02:40:15 +0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,17 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'react-native-replacer',
+        {
+          'replacements': {
+            'console.log': function () {
+              return null;
+            },
+          },
+        },
+      ],
+    ],
   };
 };
